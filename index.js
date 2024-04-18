fa6892e89d08d36ae57d0d7cf62cd8fe6407c41d
Beyla Irakoze <beylaruz@gmail.com>
2024-04-18 10:36:07 +0200

    DOM manipulation

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
+var todoForm = document.getElementById('form');
+var todoInput = document.getElementById('input');
+var todoList = document.getElementById('list');
 
-    var todoForm = document.getElementById('form');
-    var todoInput = document.getElementById('input');
-    var todoList = document.getElementById('list');
-
-    todoForm.addEventListener('submit', function() {
-        if (todoInput.value.trim() === '') return;
+todoForm.addEventListener('submit', function(event) { 
+    event.preventDefault();
+    if (todoInput.value !== ''){
         addTodo(todoInput.value);
         todoInput.value = '';
-    });
-
-    function addTodo(text) {
-        const li = document.createElement('li');
-        li.textContent = text;
-        const deleteButton = document.createElement('button');
-        deleteButton.textContent = 'Delete';
-        deleteButton.classList.add('delete-btn');
-        deleteButton.addEventListener('click', function() {
-            todoList.removeChild(li);
-        });
-        li.appendChild(deleteBtn);
-        todoList.appendChild(li);
     }
-   
+    else {
+        alert('Please enter a task to be added!');
+    }
+});
 
+function addTodo(text) {
+    let li = document.createElement('li');
+    li.textContent = text;
+    let deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.classList.add('delete-btn');
+    deleteButton.addEventListener('click', function() {
+        todoList.removeChild(li);
+    });
+    li.appendChild(deleteButton);
+    todoList.appendChild(li);
+}
